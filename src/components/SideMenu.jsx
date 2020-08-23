b781ddcc7ccfa49cc0fa90463fa74a9247a240e3
Syauqi Rahmat Sugara <syauqi@sepulsa.com>
2020-08-23 19:51:18 +0700

    feat(frontend):add menu sidebar

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bulma-components';
+import profil from '../user.png';
 
 const SideMenu = (props) => {
+  const username = 'Salis Mahmudah';
   return (
     <div className="tile is-parent is-2">
       <article className="tile is-child notification is-black">
         <div className="content">
-          <p className="title">Menu</p>
+          <p className="title has-text-light">Menu</p>
+          <img
+            style={{ height: '90px' }}
+            src={profil}
+            alt="Bulma: a modern CSS framework based on Flexbox"
+          />
+          <Dropdown color="black" label={username}>
+            <Dropdown.Item value="">
+              <Link
+                className="button is-danger is-fullwidth"
+                to="/"
+                style={{ textDecoration: 'none' }}
+              >
+                Logout
+              </Link>
+            </Dropdown.Item>
+          </Dropdown>
           <div className="tile is-vertical is-parent">
             <Link
-              className="button is-warning is-outlined"
+              className="button is-danger is-outlined"
               to="/home/statistic"
               style={{ textDecoration: 'none' }}
             >
@@ -17,7 +36,7 @@ const SideMenu = (props) => {
             </Link>
             <br />
             <Link
-              className="button is-warning is-outlined"
+              className="button is-danger is-outlined"
               to="/home/test-case"
               style={{ textDecoration: 'none' }}
             >
@@ -25,7 +44,7 @@ const SideMenu = (props) => {
             </Link>
             <br />
             <Link
-              className="button is-warning is-outlined"
+              className="button is-danger is-outlined"
               to="/home/test-run"
               style={{ textDecoration: 'none' }}
             >
@@ -33,20 +52,11 @@ const SideMenu = (props) => {
             </Link>
             <br />
             <Link
-              className="button is-warning is-outlined"
+              className="button is-danger is-outlined"
               to="/home/report"
               style={{ textDecoration: 'none' }}
             >
               Report
-            </Link>
-            <br />
-            <br />
-            <Link
-              className="button is-danger is-outlined"
-              to="/login"
-              style={{ textDecoration: 'none' }}
-            >
-              Logout
             </Link>
           </div>
         </div>
